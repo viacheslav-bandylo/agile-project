f226eef9e4792ee16c92cba04f4db8fbd213e09a
Viascheslav Bandylo <viacheslav.bandylo@yandex.ru>
2024-08-15 14:04:13 +0400

    Lesson 1. Task 6

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from apps.tasks.views.tag_views import TagListAPIView
-
+from apps.tasks.views.tag_views import TagListAPIView, TagDetailAPIView
 
 urlpatterns = [
     path('tags/', TagListAPIView.as_view()),
+    path('tags/<int:pk>/', TagDetailAPIView.as_view()),
 ]
--- apps/tasks/views/tag_views.py
+++ apps/tasks/views/tag_views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework import status
@@ -40,4 +41,48 @@ class TagListAPIView(APIView):
         return Response(
             serializer.errors,
             status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class TagDetailAPIView(APIView):
+    def get_object(self, pk: int) -> Tag:
+        return get_object_or_404(Tag, pk=pk)
+
+    def get(self, request: Request, pk: int) -> Response:
+        tag = self.get_object(pk=pk)
+
+        serializer = TagSerializer(tag)
+
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK,
+        )
+
+    def put(self, request: Request, pk: int) -> Response:
+        tag = self.get_object(pk=pk)
+        serializer = TagSerializer(tag, data=request.data)
+
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+
+            return Response(
+                serializer.validated_data,
+                status=status.HTTP_200_OK,
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    def delete(self, request: Request, pk: int) -> Response:
+        tag = self.get_object(pk=pk)
+
+        tag.delete()
+
+        return Response(
+            data={
+                "message": "Tag was deleted successfully"
+            },
+            status=status.HTTP_200_OK
         )
