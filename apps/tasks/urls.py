ff863eb2908e744aaaa92a54622e6a908451afd3
Viascheslav Bandylo <viacheslav.bandylo@yandex.ru>
2024-08-16 14:21:10 +0400

    Lesson 2. Task 2

--- apps/tasks/urls.py
+++ apps/tasks/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from apps.tasks.views.tag_views import TagListAPIView, TagDetailAPIView
+from apps.tasks.views.task_views import TasksListAPIView
 
 urlpatterns = [
+    path('', TasksListAPIView.as_view()),
     path('tags/', TagListAPIView.as_view()),
     path('tags/<int:pk>/', TagDetailAPIView.as_view()),
 ]
